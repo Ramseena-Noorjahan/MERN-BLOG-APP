ec515d490c06410706fed7c9f1ed525458052d19
Salah Ashraf <salahashraf3@gmail.com>
2024-03-25 12:29:58 +0530

    added cors url

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ const app = express();
 
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "https://mern-blog-app-frontend-rho.vercel.app" }));
 app.use(upload())
 app.use('/uploads',express.static(__dirname + '/uploads'))
 
